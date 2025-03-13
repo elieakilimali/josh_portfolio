d42279bb249b0597ac973d52b23658dd79814dcb
elieaz <elieazdev@gmail.com>
2025-03-13 11:43:40 +0200

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary1: "#0D1B2A",
+        primary2: "#1B263B",
+        primary3: "#415A77",
+        primary4: "#778DA9",
+        primary5: "#E0E1DD",
+
+        secondary: "#9333EA",
+        accent: "#14B8A6",
+      },
+    },
+  },
+  plugins: [],
+}
